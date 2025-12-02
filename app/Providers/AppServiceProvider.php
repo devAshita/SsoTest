b343c3a65f68599ad0f887637e785350c441f5ea
abe <abe@ashitalab.co.jp>
2025-12-02 21:12:05 +0900

    R)fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,6 +17,13 @@ class AppServiceProvider extends ServiceProvider
         if (config('oidc.is_idp')) {
             Passport::tokensExpireIn(now()->addSeconds(config('oidc.idp.access_token_lifetime')));
             Passport::refreshTokensExpireIn(now()->addSeconds(config('oidc.idp.refresh_token_lifetime')));
+            
+            // OIDCスコープを定義
+            Passport::tokensCan([
+                'openid' => 'OpenID Connect認証',
+                'profile' => 'プロフィール情報へのアクセス',
+                'email' => 'メールアドレスへのアクセス',
+            ]);
         }
     }
 }
